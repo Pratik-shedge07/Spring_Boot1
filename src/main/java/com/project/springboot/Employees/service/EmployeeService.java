dbf1248c6560417a67c9c0d3201105c87e91cb59
Pratik-shedge07 <shedgepratik999@gmail.com>
2025-08-03 12:41:47 +0530

    services added

--- src/main/java/com/project/springboot/Employees/service/EmployeeService.java
+++ src/main/java/com/project/springboot/Employees/service/EmployeeService.java
@@ -1,10 +1,21 @@
 package com.project.springboot.Employees.service;
 
 import com.project.springboot.Employees.entity.Employee;
+import com.project.springboot.Employees.request.EmployeeRequest;
 
 import java.util.List;
 
 public interface EmployeeService {
 
     List<Employee> findAll();
+
+    Employee findById(long theId);
+
+    Employee save(EmployeeRequest theEmployeeRequest);
+
+    Employee update(long id, EmployeeRequest employeeRequest);
+
+    Employee convertToEmployee(long id, EmployeeRequest employeeRequest);
+
+    void deleteById(long theId);
 }
