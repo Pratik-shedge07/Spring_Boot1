1881a52ff91d5eecb1cd5fa239801017f227d2eb
Pratik-shedge07 <shedgepratik999@gmail.com>
2025-08-03 12:43:38 +0530

    service implementation added

--- src/main/java/com/project/springboot/Employees/service/EmployeeServiceImpl.java
+++ src/main/java/com/project/springboot/Employees/service/EmployeeServiceImpl.java
@@ -2,7 +2,9 @@ package com.project.springboot.Employees.service;
 
 import com.project.springboot.Employees.dao.EmployeeDAO;
 import com.project.springboot.Employees.entity.Employee;
+import com.project.springboot.Employees.request.EmployeeRequest;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -18,5 +20,36 @@ public class EmployeeServiceImpl implements EmployeeService {
     @Override
     public List<Employee> findAll() {
         return employeeDAO.findAll();
+    }
+
+    @Override
+    public Employee findById(long theId) {
+        return employeeDAO.findById(theId);
+    }
+
+    @Transactional
+    @Override
+    public Employee save(EmployeeRequest theEmployeeRequest) {
+        Employee employee = convertToEmployee(0, theEmployeeRequest);
+        return employeeDAO.save(employee);
+    }
+
+    @Transactional
+    @Override
+    public Employee update(long id, EmployeeRequest employeeRequest) {
+        Employee employee = convertToEmployee(id, employeeRequest);
+        return employeeDAO.save(employee);
+    }
+
+    @Override
+    public Employee convertToEmployee(long id, EmployeeRequest employeeRequest) {
+
+        return new Employee(id, employeeRequest.getFirstName(), employeeRequest.getLastName(), employeeRequest.getEmail());
+    }
+
+    @Transactional
+    @Override
+    public void deleteById(long theId) {
+        employeeDAO.deleteById(theId);
     }
 }
